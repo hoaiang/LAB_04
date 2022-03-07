8b97d82aebfc9991897c85cb84ec21cea78a6f7f
hoaiang <tranhoaian01011990@gmail.com>
2022-03-07 08:46:36 +0700

    thêm mật khẩu

--- FromDangnhap/FromDangnhap/Form1.Designer.cs
+++ FromDangnhap/FromDangnhap/Form1.Designer.cs
@@ -31,31 +31,59 @@ namespace FromDangnhap
         {
             this.lblUserName = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtmk = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblUserName
             // 
             this.lblUserName.AutoSize = true;
-            this.lblUserName.Location = new System.Drawing.Point(211, 157);
+            this.lblUserName.Location = new System.Drawing.Point(281, 193);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUserName.Name = "lblUserName";
-            this.lblUserName.Size = new System.Drawing.Size(57, 13);
+            this.lblUserName.Size = new System.Drawing.Size(75, 17);
             this.lblUserName.TabIndex = 0;
             this.lblUserName.Text = "UserName";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(301, 154);
+            this.txtUserName.Location = new System.Drawing.Point(401, 190);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(251, 20);
+            this.txtUserName.Size = new System.Drawing.Size(333, 22);
             this.txtUserName.TabIndex = 1;
+            // 
+            // txtmk
+            // 
+            this.txtmk.Location = new System.Drawing.Point(401, 271);
+            this.txtmk.Margin = new System.Windows.Forms.Padding(4);
+            this.txtmk.Name = "txtmk";
+            this.txtmk.Size = new System.Drawing.Size(333, 22);
+            this.txtmk.TabIndex = 3;
+            this.txtmk.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtmk.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 274);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mật khẩu";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.txtmk);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.lblUserName);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -67,6 +95,8 @@ namespace FromDangnhap
 
         private System.Windows.Forms.Label lblUserName;
         private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtmk;
+        private System.Windows.Forms.Label label1;
     }
 }
 
